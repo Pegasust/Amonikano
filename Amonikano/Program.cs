38c43c3c9ce6d4c575fb08b7c81ae9976a87e55a
Hung Tran <pegasucksgg@gmail.com>
2018-08-10 01:31:56 +0700

    Hello, this is the first version of amonikano

---
+++ Amonikano/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Amonikano
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
